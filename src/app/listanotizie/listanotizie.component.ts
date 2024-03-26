fe738443b7e2bddb2712209a58d17ba3c4697a9e
pieramicileonardo <pieramicileonardo@gmail.com>
2024-03-26 17:25:07 +0100

    Lista notizie

--- src/app/listanotizie/listanotizie.component.ts
+++ src/app/listanotizie/listanotizie.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { News } from 'src/app/models/News';
+import { ListanotizieService } from './listanotizie.service';
 
 @Component({
   selector: 'app-listanotizie',
   templateUrl: './listanotizie.component.html',
   styleUrls: ['./listanotizie.component.css']
 })
-export class ListanotizieComponent {
+export class ListanotizieComponent implements OnInit {
+  notizie: News[] = [];
 
+  constructor(private listanotizieService: ListanotizieService) { }
+
+  ngOnInit(): void {
+    this.caricaNotizie();
+  }
+
+  caricaNotizie(): void {
+    this.listanotizieService.getNotizie()
+      .subscribe(notizie => {
+        this.notizie = notizie;
+      });
+  }
 }
+
+
