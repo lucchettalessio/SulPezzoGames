37956d143239b3805236563b6fb1371ef604f54a
lucchettalessio <lucche.alessio@gmail.com>
2024-03-26 10:23:50 +0100

    component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { ListanotizieComponent } from './listanotizie/listanotizie.component';
 import { ListarecensioniComponent } from './listarecensioni/listarecensioni.component';
 import { ListavideogiochiComponent } from './listavideogiochi/listavideogiochi.component';
 import { LoginformComponent } from './loginform/loginform.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [{
   //http://localhost:4200/loginpage
@@ -24,6 +25,11 @@ const routes: Routes = [{
   //http://localhost:4200/listavideogiochi
   path: 'listavideogiochi',
   component: ListavideogiochiComponent
+},
+{
+  //http://localhost:4200/homepage
+  path: 'homepage',
+  component: HomepageComponent
 }];
 
 @NgModule({
