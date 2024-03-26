b533c8aee28640f971e7b1f63ca189501c8b95dc
Mark-ito <markopruiti@gmail.com>
2024-03-26 09:48:03 +0100

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,40 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginformComponent } from './loginform/loginform.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { UtenteloggatoComponent } from './utenteloggato/utenteloggato.component';
 import { ListavideogiochiComponent } from './listavideogiochi/listavideogiochi.component';
 import { ListarecensioniComponent } from './listarecensioni/listarecensioni.component';
 import { ListanotizieComponent } from './listanotizie/listanotizie.component';
+import { Routes } from '@angular/router';
+
+const routes : Routes = [
+  {
+    //http://localhost:4200/loginpage
+    path: 'loginpage',
+    component: LoginformComponent
+  },
+  {
+    //http://localhost:4200/listanotizie
+    path: 'listanotizie',
+    component: ListanotizieComponent
+  },
+  {
+    //http://localhost:4200/listarecensioni
+    path: 'listarecensioni',
+    component: ListarecensioniComponent
+  },
+  {
+    //http://localhost:4200/listavideogiochi
+    path: 'listavideogiochi',
+    component: ListavideogiochiComponent
+  },
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginformComponent,
     HomepageComponent,
-    UtenteloggatoComponent,
     ListavideogiochiComponent,
     ListarecensioniComponent,
     ListanotizieComponent
