4b1118ad419de75dfffa0cf7f3e24d9f09fbabea
Mark-ito <markopruiti@gmail.com>
2024-03-29 15:44:44 +0100

    modello recensione ts

--- src/app/models/Recensione.ts
+++ src/app/models/Recensione.ts
@@ -1,3 +1,6 @@
+import { Utente } from "./Utente";
+import { Videogioco } from "./Videogioco";
+
 export interface Recensione{
     id:number,
     titolo:string,
@@ -5,8 +8,6 @@ export interface Recensione{
     punteggio:number,
     immagine:string,
     testo:string,
-    autore:number,
-    videogioco:number,
-    nome:String,
-    cognome:String
+    autore:Utente,
+    videogioco:Videogioco,
 }
