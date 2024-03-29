ce3bb0d0c9d50789c1d0d043cdfd6ba043e95148
lucchettalessio <lucche.alessio@gmail.com>
2024-03-29 16:03:49 +0100

    login pulsante

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './app.component-dark.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular';
   darkMode = false;
+  token?:string;
 
 
   switchDarkMode(){
     this.darkMode = !this.darkMode;
+
+  }
+
+  ngOnInit(): void {
+    this.token =  sessionStorage.getItem("token")?.split("-")[0];
+    console.log(this.token);
+  }
+
+  clearToken(){
+    sessionStorage.removeItem("token");
   }
 
 }
