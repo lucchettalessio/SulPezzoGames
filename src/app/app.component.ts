8eeccd5dca8e0dd9476c45d5553e77882b5e11c8
mihaelamiky <mihaela.cipriano@gmail.com>
2024-04-04 12:25:20 +0200

    pulizia fil residui darkmode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css', './app.component-dark.css']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
   title = 'angular';
