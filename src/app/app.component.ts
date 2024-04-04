51fbd2584d8a19ccc38fba626b9c45ceb4617be4
mihaelamiky <mihaela.cipriano@gmail.com>
2024-04-04 12:23:29 +0200

    eliminazione darkMode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,35 +9,16 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 })
 export class AppComponent implements OnInit{
   title = 'angular';
-  darkMode = false;
-
   token: string | null | undefined;
   ruolo: string = "";
-
   utente! : Utente;
-
   userPopUp: boolean = false;
-
-
 
   constructor(private http : HttpClient){
     this.http = http;
   }
 
-  switchDarkMode(){
-    this.darkMode = !this.darkMode;
-    if(this.darkMode) {
-      // Salva la modalità notte nella sessione o local storage se necessario
-      localStorage.setItem('darkMode', 'true');
-    } else {
-      // Rimuovi la modalità notte dalla sessione o local storage se necessario
-      localStorage.removeItem('darkMode');
-    }
-  }
   ngOnInit(): void {
-    // Controlla se la modalità notte è stata attivata in precedenza
-    this.darkMode = localStorage.getItem('darkMode') === 'true';
-
     this.token = sessionStorage.getItem("token");
     if(this.token != null){
       this.ruolo = this.token.split("-")[0]
