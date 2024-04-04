53c56502f6d72dad1ab47991bc12c227c5ccf993
pieramicileonardo <pieramicileonardo@gmail.com>
2024-04-04 10:01:38 +0200

    elmina vg

--- src/app/listavideogiochi/listavideogiochi.component.ts
+++ src/app/listavideogiochi/listavideogiochi.component.ts
@@ -4,6 +4,8 @@ import { Videogioco } from 'src/app/models/Videogioco';
 import { ListarecensioniComponent } from '../listarecensioni/listarecensioni.component';
 import { Router, RouterModule } from '@angular/router';
 import { EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-listavideogiochi',
@@ -16,9 +18,18 @@ export class ListavideogiochiComponent implements OnInit {
   rowsPerPage: number = 4; 
   currentPage: number = 1;
   totalPages: number | undefined;
+  isDeleting: boolean = false;
+  deletingId: number = -1;
+
+  isModifying: boolean = false;
+  modifyingId: number = -1;
+  formModifica! : FormGroup;
+
+  isInserting: boolean = false;
+  formInsert! : FormGroup;
   @Output() filterRecensioniEvent = new EventEmitter<number>();
 
-  constructor(private videogiochiService: VideogiochiService, private router : Router) { }
+  constructor(private videogiochiService: VideogiochiService, private router : Router, private http : HttpClient) { }
 
   ngOnInit(): void {
     this.getVideogiochi();
@@ -51,6 +62,51 @@ export class ListavideogiochiComponent implements OnInit {
 
   openRecensioni(idVideogioco: number) {
     this.filterRecensioniEvent.emit(idVideogioco);
+  }
+
+  elimina(id: number){
+    this.isModifying = false;
+    this.isInserting = false;
+    this.isDeleting = true;
+    this.deletingId = id;
+  }
+
+  confermaElimina(id: number){
+    var token = sessionStorage.getItem("token");
+    if(token == null){
+      token = "";
+    }
+
+    const headers = new HttpHeaders(
+      {
+        'Content-type' : 'application/json',
+        'token': token as string
+      }
+    );
+
+    const params = new HttpParams().set("id", id);
+
+    this.http.get("http://localhost:8080/api/Videogioco/delete", {headers, params}).subscribe(risposta =>{
+      var check = risposta as boolean;
+      if(check){
+        alert("Eliminazione avvenuta con successo")
+        // window.location.href = "/areadirigenti"
+        var indice = this.videogiochi?.findIndex(x => x.id === id)
+        console.log("indice", indice)
+        if(indice! > -1){
+          this.videogiochi?.splice(indice!, 1);
+        }
+      }
+
+    })
+
+    this.isDeleting = false;
+    this.deletingId = -1;
+  }
+  
+  annullaElimina(){
+    this.isDeleting = false;
+    this.deletingId = -1;
   }
 
 }
